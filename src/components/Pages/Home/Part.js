ab291a7fa543da89ee5d2db7455578fcd28b9069
Mostafizur rahman <sourav10681@gmail.com>
2022-05-23 02:15:04 +0600

    parts section added

---
+++ src/components/Pages/Home/Part.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Part = ({ part }) => {
+    const { name, img, description, minimum, quantity, price } = part;
+    return (
+        <div class="card w-96 bg-base-100 shadow-xl">
+            <figure>
+                <img width='450px' height='300px' src={img} alt={img} class="rounded-xl" />
+            </figure>
+            <div class="card-body">
+                <h2 class="card-title text-blue-900 uppercase">{name}</h2>
+                <p>{description}</p>
+                <p><small>Mimimum Order: {minimum}</small></p>
+                <p><small>Quantity: {quantity}</small></p>
+                <p>Price: <span className='text-yellow-500'>${price}</span></p>
+                <div class="card-actions justify-end">
+                    <button class="btn bg-blue-900">Get Started</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Part;
--- src/components/Pages/Home/Parts.js
+++ src/components/Pages/Home/Parts.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Part from './Part';
 
 const Parts = () => {
-    return (
-        <div>
+    const [parts, setParts] = useState([]);
 
+    useEffect(() => {
+        fetch('parts.json')
+            .then(res => res.json())
+            .then(data => setParts(data))
+    }, [])
+    return (
+        <div className='px-8 mt-24'>
+            <h2 className='text-5xl font-bold text-center mb-8'>Our Available <span className='text-blue-900'>Parts</span></h2>
+
+            <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 grid-cols-1 gap-5'>
+                {
+                    parts.map(part => <Part
+                        key={part._id}
+                        part={part}
+                    ></Part>)
+                }
+            </div>
         </div>
     );
 };
