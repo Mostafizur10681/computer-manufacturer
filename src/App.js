c0a5b59320e987c9f847397cf4bc164da690c120
Mostafizur rahman <sourav10681@gmail.com>
2022-05-23 01:30:25 +0600

    navbar & banner added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Pages/Shared/Navbar';
+import Footer from './components/Pages/Shared/Footer';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Pages/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
